581f929aec77cacdae746da37aaaba2d842c6085
Karl Balemi <km_balemi@hotmail.com>
2015-04-24 19:12:14 +1000

    More burgers and order by votes.

--- app/scripts/controllers/burger.js
+++ app/scripts/controllers/burger.js
@@ -17,12 +17,13 @@ angular.module('bestInMelbourneApp')
           name : 'royalebrothers.com.au/',
           link : 'http://royalebrothers.com.au/'
         },
-        social : {
-          instagram : {
-            name : 'theroyalebros',
-            link : 'https://instagram.com/theroyalebros/'
+        social : [
+          {
+            channel: 'instagram',
+            name: 'theroyalebros',
+            link: 'https://instagram.com/theroyalebros/'
           }
-        },
+        ],
         venues: [
           {
             address: 'Rear 1 Church Street',
@@ -39,18 +40,65 @@ angular.module('bestInMelbourneApp')
           name : 'grandtrailerpark.com.au/',
           link : 'http://www.grandtrailerpark.com.au/'
         },
-        social : {
-          instagram : {
-            name : 'grandtrailerpark',
-            link : 'https://instagram.com/grandtrailerpark/'
+        social : [
+          {
+            channel: 'instagram',
+            name: 'grandtrailerpark',
+            link: 'https://instagram.com/grandtrailerpark/'
           }
-        },
+          ],
         venues : [
           {
             address: '87 Bourke Street',
             suburb: 'Melbourne',
             state: 'VIC',
             postcode: '3000'
+          }
+        ]
+      },
+      {
+        title : 'Humburger',
+        votes : 4,
+        website : {
+          name : 'humburger.com.au/',
+          link : 'http://www.humburger.com.au/'
+        },
+        social : [
+          {
+            channel : 'instagram',
+            name : 'humburgermelb',
+            link : 'https://instagram.com/humburgermelb/'
+          }
+        ],
+        venues : [
+          {
+            address: '789 Glenferrie Road',
+            suburb: 'Hawthorn',
+            state: 'VIC',
+            postcode: '3122'
+          }
+        ]
+      },
+      {
+        title : 'Brother Burger and The Marvellous Brew',
+        votes : 8,
+        website : {
+          name : 'www.brotherburger.com.au/',
+          link : 'http://www.brotherburger.com.au/'
+        },
+        social : [
+          {
+            channel : 'facebook',
+            name : 'Brother Burger and The Marvellous Brew',
+            link : 'http://www.facebook.com/brotherburger'
+          }
+        ],
+        venues : [
+          {
+            address: '413 Brunswick Street',
+            suburb: 'Fitzroy',
+            state: 'VIC',
+            postcode: '3065'
           }
         ]
       }
