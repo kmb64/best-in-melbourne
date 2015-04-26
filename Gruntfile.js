8a198348f0753ab68f768dad5e8ae79b8fece462
kmb64 <km_balemi@hotmail.com>
2015-04-26 23:35:32 +1000

    Get Instagram/Facebook profile pictures.

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,7 +66,6 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        protocol: 'https',
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: '*',
--- app/scripts/controllers/burger.js
+++ app/scripts/controllers/burger.js
@@ -18,16 +18,26 @@ angular.module('bestInMelbourneApp')
       $scope.places = places;
       angular.forEach($scope.places, function(val){
         console.log(val.social[0].userId);
-        //$http.get('https://api.instagram.com/v1/users/'+val.social[0].userId+'?client_id=0a5a8e73d67249a8bb13f00948c9a864')
+        $http.jsonp('https://api.instagram.com/v1/users/'+val.social[0].userId+'?client_id=0a5a8e73d67249a8bb13f00948c9a864&callback=JSON_CALLBACK')
+          .success(function(response){
+            //console.log(response.data['profile_picture']);
+            if(response.data){
+              val.profilePicture = response.data['profile_picture'];
+            }
+            //try facebook
+            else {
+              $http.jsonp('https://graph.facebook.com/v2.3/' + val.social[0].userId + '/picture?redirect=false&width=150&callback=JSON_CALLBACK')
+                .success(function(fbResponse){
+                  val.profilePicture = fbResponse.data['url'];
+                })
+            }
+
+          });
+        //$http.get('https://api.instagram.com/v1/users/'+val.social[0].userId+'/media/recent/?client_id=0a5a8e73d67249a8bb13f00948c9a864')
         //  .success(function(data){
         //    console.log(data.profile_picture);
         //
         //  });
-        $http.get('https://api.instagram.com/v1/users/'+val.social[0].userId+'/media/recent/?client_id=0a5a8e73d67249a8bb13f00948c9a864')
-          .success(function(data){
-            console.log(data.profile_picture);
-
-          });
       });
     });
 
