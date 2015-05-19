c48b9ff7cfa8d474ec2db40c4f9aaef67aa46d00
Karl Balemi <karl.balemi@auspost.com.au>
2015-05-19 17:27:59 +1000

    Install jasmine to run unit tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -396,6 +396,33 @@ module.exports = function (grunt) {
           '.tmp/styles/main.css': '<%= yeoman.app %>/styles/main.scss'
         }
       }
+    },
+
+    jasmine: {
+      options: {
+        specs: 'test/spec/**/*.js',
+        vendor: [
+          'bower_components/angular/angular.js',
+          'bower_components/angular-animate/angular-animate.js',
+          'bower_components/angular-cookies/angular-cookies.js',
+          'bower_components/angular-resource/angular-resource.js',
+          'bower_components/angular-route/angular-route.js',
+          'bower_components/angular-sanitize/angular-sanitize.js',
+          'bower_components/angular-touch/angular-touch.js',
+          'bower_components/firebase/firebase.js',
+          'bower_components/angularfire/dist/angularfire.js',
+          'bower_components/underscore/underscore.js',
+          'bower_components/angular-mocks/angular-mocks.js'
+        ]
+      },
+      test: {
+        src: [
+          'app/scripts/**/*.js'
+        ],
+        options: {
+          keepRunner: true
+        }
+      }
     }
   });
 
@@ -422,12 +449,7 @@ module.exports = function (grunt) {
   });
 
   grunt.registerTask('test', [
-    'clean:server',
-    'wiredep',
-    'concurrent:test',
-    'autoprefixer',
-    'connect:test',
-    'karma'
+    'jasmine'
   ]);
 
   grunt.registerTask('build', [
