403f7eae19c4fd813fe6e890845e9032cfdc7017
kmb64 <km_balemi@hotmail.com>
2015-05-20 00:45:19 +1000

    Introduce coverage statistics.

--- Gruntfile.js
+++ Gruntfile.js
@@ -400,7 +400,18 @@ module.exports = function (grunt) {
           'app/scripts/**/*.js'
         ],
         options: {
-          keepRunner: true
+          keepRunner: true,
+          template: require('grunt-template-jasmine-istanbul'),
+          templateOptions: {
+            coverage: 'bin/coverage/coverage.json',
+            report: 'bin/coverage',
+            thresholds: {
+              lines: 75,
+              statements: 75,
+              branches: 75,
+              functions: 90
+            }
+          }
         }
       }
     },
