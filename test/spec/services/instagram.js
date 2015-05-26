94e4f4f1f69816a532c72e4fd712bad33bb2e703
Karl Balemi <karl.balemi@auspost.com.au>
2015-05-26 16:39:58 +1000

    Instagram service specs.

---
+++ test/spec/services/instagram.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe('Instagram Service', function () {
+
+  beforeEach(module('bestInMelbourneApp'));
+
+  beforeEach(function () {
+
+  });
+
+  it('should call an instagram endpoint to retrieve a place\'s profile picture', inject(function () {
+
+  }));
+
+});
