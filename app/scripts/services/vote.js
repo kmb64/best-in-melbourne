2124785c022a3d93bd77d2d9a5e1a5464e74b671
Karl Balemi <karl.balemi@auspost.com.au>
2015-06-02 12:18:44 +1000

    Start redefining vote service.

--- app/scripts/services/vote.js
+++ app/scripts/services/vote.js
@@ -1,31 +1,11 @@
 'use strict';
 
 angular.module('bestInMelbourneApp')
-  .service('Vote', ['$window', function ($window) {
+  .factory('vote', [ function () {
 
-    var updateVote = function (place) {
-      if(hasLocalStorage() && !hasVoted()) {
-        place.votes += 1;
-        setVoted();
-      }
-      return place;
-    };
+    var voteService = {};
 
-    var hasLocalStorage = function(){
-      return ($window.localStorage);
-    };
+    return voteService;
 
-    var hasVoted = function(){
-      return $window.localStorage.getItem('burger');
-    };
-
-    var setVoted = function(){
-      return $window.localStorage.setItem('burger', {'voted': 1});
-    };
-
-
-    return {
-      updateVote: updateVote
-    };
 
   }]);
--- test/spec/services/vote.js
+++ test/spec/services/vote.js
@@ -2,43 +2,43 @@
 
 describe('Service: VoteService', function () {
 
-  var place, mockWindow;
+  var mockWindow;
 
   beforeEach(module('bestInMelbourneApp'));
 
   beforeEach(function(){
-    place = {
-      votes : 0
-    };
+
+    //mockFirebaseArray = function(){
+    //  return {
+    //    $loaded : function(){
+    //
+    //    }
+    //  }
+    //};
 
     mockWindow = {
-      localStorage: {
-        getItem: function () {},
-        setItem: function () {}
-      }
+      Firebase : function(){}
     };
 
     module(function ($provide) {
-      $provide.value('$window', mockWindow);
+      $provide.constant('$window', mockWindow);
     });
+
+    //need user id, place id --> website or facebook id?? place type
+
+    //first need to check if that user id has voted for place id before
+
+    //if they have return message ==> Looks like you've already voted for this place!
+
+    //if they haven't save place id in there favourites update place vote count return message --> Thanks for voting!
 
   });
 
-  it('should increment the number of votes for a burger place', inject(function (Vote) {
-    Vote.updateVote(place);
-    expect(place.votes).toEqual(1);
-  }));
+  it('should check if a user has voted for a particular place before', inject(function (vote, $window) {
+    spyOn($window.Firebase);
+    vote.hasUserVotedForPlace('useId1234', 'placeId1234', 'burgerPlaceType');
+    expect($firebaseArray.toHaveBeenCalledWith)
 
-  it('should not update a places vote if the user doesn\'t have local storage', inject(function(Vote, $window){
-    $window.localStorage = false;
-    Vote.updateVote(place);
-    expect(place.votes).toBe(0);
-  }));
-
-  it('should not update the votes if a user has already voted', inject(function(Vote, $window){
-    spyOn($window.localStorage, 'getItem').and.returnValue(true);
-    Vote.updateVote(place);
-    expect(place.votes).toBe(0);
   }));
 
 });
