004d4f89bd526a99aa92095482344552ffc1609e
kmb64 <km_balemi@hotmail.com>
2015-06-03 21:21:28 +1000

    Instagram get profile picture service.

--- app/scripts/services/instagram.js
+++ app/scripts/services/instagram.js
@@ -1,17 +1,26 @@
 'use strict';
 
 angular.module('bestInMelbourneApp')
-  .factory('instagram', ['$http', 'config', function ($http, config) {
+  .factory('instagram', ['$http', 'config', '$q', function ($http, config, $q) {
 
     var instagram = {};
 
     var params = {
-      'callback' : 'JSON_CALLBACK',
-      'client_id' : config.instagramClientId
+      'callback': 'JSON_CALLBACK',
+      'client_id': config.instagramClientId
     };
 
-    instagram.getProfilePicture = function(id){
-      return $http.jsonp(config.instagram + id, params);
+    instagram.getProfilePicture = function (id) {
+      var deferred = $q.defer();
+
+      $http.jsonp(config.instagram + id, {params: params}).then(function(response){
+        deferred.resolve(response.data.data.profile_picture);
+      }, function(){
+        deferred.resolve('default image link?')
+      });
+
+      return deferred.promise;
+
     };
 
     return instagram;
--- test/spec/services/instagram.js
+++ test/spec/services/instagram.js
@@ -1,36 +1,56 @@
 'use strict';
 
-describe('Facebook Service', function () {
+describe('Instagram Service', function () {
 
-  var mockDeferred;
+  var mockPromise, successCallback, failCallback;
 
   beforeEach(module('bestInMelbourneApp'));
 
   beforeEach(function () {
-
-    mockDeferred = {
-      reject: function () {
-      },
-      resolve: function () {
-      },
-      promise: {}
-    };
+    mockPromise = {
+      then : function(success, fail) {
+        successCallback = success;
+        failCallback = fail;
+      }
+    }
   });
 
-  it('should call the correct instagram api endpoint to retrieve a place\'s profile picture', inject(function (instagram, $http, config) {
+  it('should call the correct instagram api endpoint to retrieve a place\'s profile picture',
+    inject(function (instagram, $http, config) {
     var id = '1234';
-    spyOn($http, 'jsonp');
+    spyOn($http, 'jsonp').and.returnValue(mockPromise);
     instagram.getProfilePicture(id);
     expect($http.jsonp).toHaveBeenCalledWith(config.instagram + id, jasmine.any(Object));
   }));
 
   it('should call the instagram api with the correct parameters', inject(function (instagram, $http, config) {
-    spyOn($http, 'jsonp');
+    spyOn($http, 'jsonp').and.returnValue(mockPromise);
     instagram.getProfilePicture();
-    expect($http.jsonp).toHaveBeenCalledWith(jasmine.any(String), {
+    expect($http.jsonp).toHaveBeenCalledWith(jasmine.any(String), {params : {
       'callback' : 'JSON_CALLBACK',
       'client_id' : config.instagramClientId
+    }});
+
+  }));
+
+  it('should return a the url of the place\'s profile picture', inject(function (instagram, $http) {
+    spyOn($http, 'jsonp').and.returnValue(mockPromise);
+    var profilePic = instagram.getProfilePicture();
+    successCallback({
+      data : {
+        data : {
+          profile_picture : 'instagram.com/1234/picture'
+        }
+      }
     });
+    expect(profilePic.$$state.value).toBe('instagram.com/1234/picture');
+  }));
+
+  it('should return a default image url if the call to instagram fails for whatever reason', inject(function (instagram, $http) {
+    spyOn($http, 'jsonp').and.returnValue(mockPromise);
+    var profilePic = instagram.getProfilePicture();
+    failCallback();
+    expect(profilePic.$$state.value).toBe('default image link?');
   }));
 
 });
