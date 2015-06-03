9c8565922178a83319dd5e3621f1d3cfb8e2f75e
kmb64 <km_balemi@hotmail.com>
2015-06-03 22:25:28 +1000

    Remove unused config.

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,39 +1,19 @@
 'use strict';
 
-var instagramClientId = '0a5a8e73d67249a8bb13f00948c9a864';
-
 angular.module('bestInMelbourneApp')
   .constant('config', {
 
-    firebase : 'https://vivid-inferno-5850.firebaseio.com/',
-    instagram : 'https://api.instagram.com/v1/users/',
-    instagramClientId : instagramClientId,
+    firebase: 'https://vivid-inferno-5850.firebaseio.com/',
+    instagram: 'https://api.instagram.com/v1/users/',
+    instagramClientId: '0a5a8e73d67249a8bb13f00948c9a864',
 
-    recentMedia : {
-      instagram : function(userId) {
+    recentMedia: {
+      instagram: function (userId) {
         return {
-          endPoint : 'https://api.instagram.com/v1/users/' + userId + '/media/recent/',
-          params :{'client_id' : instagramClientId}
+          endPoint: 'https://api.instagram.com/v1/users/' + userId + '/media/recent/',
+          params: {'client_id': config.instagramClientId}
         }
       }
 
-    },
-
-    profilePicture : {
-      instagram : function(userId){
-        return {
-          endPoint : 'https://api.instagram.com/v1/users/' + userId,
-          params :{'client_id' : instagramClientId},
-          accessor : 'profile_picture'
-
-        }
-      },
-      facebook : function(userId){
-        return {
-          endPoint : 'https://graph.facebook.com/v2.3/' + userId + '/picture',
-          params : {redirect : false ,width :150},
-          accessor : 'url'
-        }
-      }
     }
   });
