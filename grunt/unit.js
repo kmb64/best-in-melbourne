fd0850bd25e242a4d1ab0ab63a25ccd840895afb
Karl Balemi <karl.balemi@auspost.com.au>
2015-06-29 13:01:47 +1000

    Organize angular scripts into module structure.

--- grunt/unit.js
+++ grunt/unit.js
@@ -6,21 +6,28 @@ module.exports = function (grunt) {
 
     jasmine: {
       options: {
-        specs: ['<%= mcac.test.base %>/specs/*.js'],
+        specs: ['<%= mcac.test.base %>/spec/{,*/}*.js'],
         vendor: [
-          'node_modules/angular/angular.js',
-          'node_modules/angular-mocks/angular-mocks.js',
-          'node_modules/angular-messages/angular-messages.js',
-          'node_modules/requirejs/require.js'
+          'bower_components/angular/angular.js',
+          'bower_components/angular-mocks/angular-mocks.js',
+          'bower_components/angular-animate/angular-animate.js',
+          'bower_components/angular-cookies/angular-cookies.js',
+          'bower_components/angular-resource/angular-resource.js',
+          'bower_components/angular-route/angular-route.js',
+          'bower_components/angular-sanitize/angular-sanitize.js',
+          'bower_components/angular-touch/angular-touch.js',
+          'bower_components/firebase/firebase.js',
+          'bower_components/angularfire/dist/angularfire.js',
+          'bower_components/underscore/underscore.js'
         ],
         helpers: [
-          '<%= mcac.app.base %>/app.js'
+          '<%= mcac.app.base %>/app.js',
+          '<%= mcac.app.base %>/config.js'
         ]
       },
       test: {
         src: [
-          '<%= mcac.app.base %>/scripts/*.js',
-          '!<%= mcac.app.base %>/scripts/qasAutoComplete.js'
+          '<%= mcac.app.base %>/scripts/{,*/}*.js'
         ],
         options: {
           keepRunner: true
